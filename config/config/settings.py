5c229f30d092ed427bd0f09c4f34eab44b4d92f9
Shubham Pathak <shubham.pathak@atomicloops.com>
2025-06-10 01:17:55 +0530

    Config: DRF + swagger setup

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
+    'api'
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
